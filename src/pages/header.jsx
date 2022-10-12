4fb7319657612e6c3366a43992a7d2ae59b3cf71
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-12 13:51:00 +0500

    add nextjs in a project

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,20 +1,56 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
+import {
+    Center,
+    Divider,
+    Icon,
+    Box,
+    Flex,
+    HStack,
+    Text,
+} from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
+import { MdLocationOn } from 'react-icons/md';
+import { FaShoppingCart, FaUserAlt } from 'react-icons/fa';
+import { Link, } from '@chakra-ui/react';
 
 const Header = () => {
-  return (
-    <Container maxW='full' bg='#571F9C' color='#ffffff'>
-   <Flex>
-    <Box>
-        
+    return (
+        <Box w={['400px', '1200px', '100%']} bg="#571F9C" h="54px" color="#ffffff">
+            <Flex justify="space-between" mx={['110px', '250px', '250px']}>
+                <Box>
+                    <HStack my="14px">
+                        <EmailIcon />
+                        <Link>
+                            <Text>info@example.com</Text>
+                        </Link>
 
-    </Box>
-    <Box>
+                    </HStack>
+                </Box>
+                <Box>
+                    <HStack my="14px">
+                        <Icon as={MdLocationOn} />
+                        <Link href="#">
+                            <Text>Branches</Text>
+                        </Link>
+                        <Center height="20px">
+                            <Divider orientation="vertical" borderColor="whiteAlpha.900" />
+                        </Center>
+                        <Icon as={FaShoppingCart} />
+                        <Link>
+                            <Text>Stories</Text>
+                        </Link>
+                        <Center height="20px">
+                            <Divider orientation="vertical" borderColor="whiteAlpha.900" />
+                        </Center>
+                        <Icon as={FaUserAlt} />
+                        <Link>
+                            <Text>Contact us</Text>
+                        </Link>
+                    </HStack>
+                </Box>
+            </Flex>
+        </Box>
+    );
+};
 
-    </Box>
-   </Flex>
-  </Container>
-  )
-}
-
-export default Header
+export default Header;
