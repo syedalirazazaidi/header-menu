f5a46b00d5325dd9edb224f67ef2e2f171004d9b
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-12 09:54:38 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Header from './pages/header';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+     <Header/>
+      {/* <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
@@ -34,7 +36,7 @@ function App() {
             </Link>
           </VStack>
         </Grid>
-      </Box>
+      </Box> */}
     </ChakraProvider>
   );
 }
---
+++ src/pages/header.jsx
@@ -0,0 +1,20 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Container maxW='full' bg='#571F9C' color='#ffffff'>
+   <Flex>
+    <Box>
+        
+
+    </Box>
+    <Box>
+
+    </Box>
+   </Flex>
+  </Container>
+  )
+}
+
+export default Header
